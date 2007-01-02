e956850cb32b3964152704e83db15fd93b157e43
Leandro Lucarella <llucarella@integratech.com.ar>
2007-01-02 20:01:53 +0000

    Fix a bug when the dispatcher destructor is working.

--- test/prio-test.cpp
+++ test/prio-test.cpp
@@ -1,5 +1,6 @@
 #include <eventxx>
 #include <iostream>
+#include <map>
 #include <csignal>
 
 using eventxx::dispatcher;
@@ -10,17 +11,22 @@ struct handler
 {
 	dispatcher& d;
 	int fds[4];
+	std::map< int, eventxx::basic_event* > events;
 	handler(dispatcher& d): d(d)
 	{
 		OSASSERT(pipe, fds);
 		OSASSERT(pipe, fds+2);
 	}
-	void operator() (int fd, short event)
+	void add(eventxx::basic_event& e)
+	{
+		events[e.fd()] = &e;
+	}
+	void operator() (int fd, eventxx::type event)
 	{
 		char buf[7];
 		OSASSERT(read, fd, buf, 7);
 		std::cout << "Read from fd " << fd << ": " << buf << "\n";
-		d.exit();
+		d.del(*events[fd]);
 	}
 };
 
@@ -30,8 +36,10 @@ int main()
 	handler h(d);
 	OSASSERT(write, h.fds[1], "hola 1", 7);
 	OSASSERT(write, h.fds[3], "hola 2", 7);
-	eventxx::event< handler > e1(h.fds[0], EV_READ, h);
-	eventxx::event< handler > e2(h.fds[2], EV_READ, h);
+	eventxx::event< handler > e1(h.fds[0], eventxx::READ, h);
+	eventxx::event< handler > e2(h.fds[2], eventxx::READ, h);
+	h.add(e1);
+	h.add(e2);
 	d.add(e1, 1);
 	d.add(e2, 0);
 	d.dispatch();
