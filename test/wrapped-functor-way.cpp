c584f2e4d45ee21a00dae09f4be54144eafef7ac
Leandro Lucarella <llucarella@integratech.com.ar>
2007-03-21 21:31:43 +0000

    Add mem_cb functor to use an arbitrary memeber function as an event handler.

---
+++ test/wrapped-functor-way.cpp
@@ -0,0 +1,36 @@
+#include <eventxx>
+#include <iostream>
+#include <csignal>
+
+using eventxx::dispatcher;
+
+struct handler
+{
+	dispatcher& d;
+	int i;
+	handler(dispatcher& d): d(d), i(0) {}
+	void handle_event(int signum, short event)
+	{
+		std::cout << ++i << " interrupts, ";
+		if (i < 5) std::cout << "keep going...\n";
+		else
+		{
+			std::cout << "done!\n";
+			d.exit();
+		}
+	}
+};
+
+typedef eventxx::mem_cb< handler, void (handler::*)(int, short) > cb_type;
+
+int main()
+{
+	dispatcher d;
+	handler h(d);
+	cb_type cb(h, &handler::handle_event);
+	eventxx::signal< cb_type > e(SIGINT, cb);
+	d.add(e);
+	d.dispatch();
+	return 0;
+}
+
